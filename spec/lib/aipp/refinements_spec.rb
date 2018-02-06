0bb7bcab387d2edbffc595f235e0332f2bdd1f0c
Sven Schwyn <schwyn@bitcetera.com>
2018-02-06 20:27:21 +0100

    Improve refinement tests

--- spec/lib/aipp/refinements_spec.rb
+++ spec/lib/aipp/refinements_spec.rb
@@ -4,49 +4,55 @@ using AIPP::Refinements
 
 describe AIPP::Refinements do
 
-  describe 'String#blank_to_nil' do
-    it "must convert blank to nil" do
-      "\n     \n         ".blank_to_nil.must_be :nil?
-    end
+  context String do
+    describe :blank_to_nil do
+      it "must convert blank to nil" do
+        "\n     \n         ".blank_to_nil.must_be :nil?
+      end
 
-    it "must leave non-blank untouched" do
-      "foobar".blank_to_nil.must_equal "foobar"
-    end
+      it "must leave non-blank untouched" do
+        "foobar".blank_to_nil.must_equal "foobar"
+      end
 
-    it "must leave non-blank with whitespace untouched" do
-      "\nfoo bar\n".blank_to_nil.must_equal "\nfoo bar\n"
-    end
-  end
-
-  describe 'NilClass#blank_to_nil' do
-    it "must return self" do
-      nil.blank_to_nil.must_be :nil?
+      it "must leave non-blank with whitespace untouched" do
+        "\nfoo bar\n".blank_to_nil.must_equal "\nfoo bar\n"
+      end
     end
   end
 
-  describe 'Array#split' do
-    it "must split at pattern" do
-      [1, 2, '---', 3, 4].split(/-+/).must_equal [[1, 2], [3, 4]]
+  context NilClass do
+    describe :blank_to_nil do
+      it "must return self" do
+        nil.blank_to_nil.must_be :nil?
+      end
     end
+  end
 
-    it "won't split arrays with no pattern matches" do
-      [1, 2, 3].split(/-+/).must_equal [[1, 2, 3]]
-    end
+  context Array do
+    describe :split do
+      it "must split at pattern" do
+        [1, 2, '---', 3, 4].split(/-+/).must_equal [[1, 2], [3, 4]]
+      end
 
-    it "must keep leading empty subarrays" do
-      ['---', 1, 2, '---', 3, 4].split(/-+/).must_equal [[], [1, 2], [3, 4]]
-    end
+      it "won't split arrays with no pattern matches" do
+        [1, 2, 3].split(/-+/).must_equal [[1, 2, 3]]
+      end
 
-    it "must keep empty subarrays in the middle" do
-      [1, 2, '---', '---', 3, 4].split(/-+/).must_equal [[1, 2], [], [3, 4]]
-    end
+      it "must keep leading empty subarrays" do
+        ['---', 1, 2, '---', 3, 4].split(/-+/).must_equal [[], [1, 2], [3, 4]]
+      end
 
-    it "must drop trailing empty subarrays" do
-      [1, 2, '---', 3, 4, '---'].split(/-+/).must_equal [[1, 2], [3, 4]]
-    end
+      it "must keep empty subarrays in the middle" do
+        [1, 2, '---', '---', 3, 4].split(/-+/).must_equal [[1, 2], [], [3, 4]]
+      end
 
-    it "won't alter empty arrays" do
-      [].split(/-+/).must_equal []
+      it "must drop trailing empty subarrays" do
+        [1, 2, '---', 3, 4, '---'].split(/-+/).must_equal [[1, 2], [3, 4]]
+      end
+
+      it "won't alter empty arrays" do
+        [].split(/-+/).must_equal []
+      end
     end
   end
 
