02b35732a68aae2ebada3fad72e5431538ce27a2
Sven Schwyn <schwyn@bitcetera.com>
2021-02-19 10:45:44 +0100

    Silence Ruby warnings

--- rakefile.rb
+++ rakefile.rb
@@ -6,7 +6,7 @@ Rake::TestTask.new do |t|
   t.libs << 'lib'
   t.test_files = FileList['spec/lib/**/*_spec.rb']
   t.verbose = false
-  t.warning = true
+  t.warning = !!ENV['TEST_WARNINGS']
 end
 
 namespace :build do
@@ -18,6 +18,12 @@ namespace :build do
       checksum = Digest::SHA512.new.hexdigest(File.read("pkg/#{gem}"))
       File.open("checksum/#{gem}.sha512", 'w') { _1.write(checksum) }
     end
+  end
+end
+
+Rake::Task[:test].enhance do
+  unless ENV['TEST_WARNINGS']
+    puts "Ruby warnings disabled, set TEST_WARNINGS environment variable to enable."
   end
 end
 
