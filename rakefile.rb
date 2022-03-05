87d89bd9e2a92a388502a91bcf1f8220a6b45c41
Sven Schwyn <schwyn@bitcetera.com>
2022-03-05 19:02:48 +0100

    Remove obsolete checksum rake task

--- rakefile.rb
+++ rakefile.rb
@@ -9,18 +9,6 @@ Rake::TestTask.new do |t|
   t.warning = !ENV['RUBYOPT']&.match?(/-W0/)
 end
 
-namespace :build do
-  desc "Build checksums of all gems in pkg directory"
-  task :checksum do
-    require 'digest/sha2'
-    Dir.mkdir('checksum') unless Dir.exist?('checksum')
-    Dir.glob('*.gem', base: 'pkg').each do |gem|
-      checksum = Digest::SHA512.new.hexdigest(File.read("pkg/#{gem}"))
-      File.open("checksum/#{gem}.sha512", 'w') { _1.write(checksum) }
-    end
-  end
-end
-
 Rake::Task[:test].enhance do
   if ENV['RUBYOPT']&.match?(/-W0/)
     puts "⚠️  Ruby warnings are disabled, remove -W0 from RUBYOPT to enable."
